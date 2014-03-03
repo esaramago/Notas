b39439ecf759e565e466081c6bed6323547075f3
esaramago <reg@esaramago.com>
2014-03-03 20:54:47 +0000

    versao 3

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var watchID = null;
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    console.log('Device ready!');
+    alert('Device ready!');
 }
 function checkLanguage() {
   	navigator.globalization.getPreferredLanguage(
